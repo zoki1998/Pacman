f91e6a5432f62ff926dfb1430a86db5032c4ba06
Andrija Trifunovic <andrijatrifunovic@yahoo.com>
2019-12-03 16:40:10 +0100

    pravljenje prozora

--- main.py
+++ main.py
@@ -1,8 +1,91 @@
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
+from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
+from PyQt5.QtGui import QPainter, QColor
+import sys
+
 from tkinter import *
 
-root = Tk()
-root.title('Pacman')
-root.iconbitmap('pacman.ico')
+
+class Window(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.tiles = [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+            0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+            0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+            0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+            0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+            0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+            0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
+            0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+            0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+            0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Pacman')
+        self.setWindowIcon(QtGui.QIcon('pacman.ico'))
+        self.resize(800, 800)
+
+        print(self.pos().x(), " ", self.pos().y())
+
+        self.drawall()
+        self.center()
+        self.show()
+
+    def drawall(self):
+
+        painter = QPainter(self)
+        rect = self.contentsRect()
+        boardTop = rect.bottom() - 800
+        self.draw(boardTop, boardTop, painter, QColor(0x6666CC))
+
+    def draw(self, x, y, painter, color):
+
+        painter.fillRect(x + 1, y + 1, 20 - 2,
+                         20 - 2, color)
+
+        #painter.setPen(color.lighter())
+        painter.drawLine(x, y + 20 - 1, x, y)
+        painter.drawLine(x, y, x + 20 - 1, y)
+
+          #  painter.setPen(color.darker())
+        painter.drawLine(x + 1, y + 20 - 1,
+                         x + 20 - 1, y + 20 - 1)
+        painter.drawLine(x + 20 - 1,
+                         y + 20 - 1, x + 20 - 1, y + 1)
+
+    def squareWidth(self):
+
+        return 20
+
+    def squareHeight(self):
+
+        return 20
+
+    def center(self):
+        """centers the window on the screen"""
+
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) / 2,
+                  (screen.height() - size.height()) / 2)
 
 
-root.mainloop()
+if __name__ == '__main__':
+    app = QApplication([])
+    window = Window()
+    sys.exit(app.exec_())
