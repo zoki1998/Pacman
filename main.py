ec50900d9cf0077c458ef5d348071f372cab874c
Andrija Trifunovic <andrijatrifunovic@yahoo.com>
2019-12-04 13:12:34 +0100

    Isccrtavanje mape

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
 import sys
-
+import demo as dm
 from tkinter import *
 
 
@@ -11,70 +11,19 @@ class Window(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.tiles = [
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-            0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
-            0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        ]
+        self.tboard = Board(self)
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle('Pacman')
+
+        self.setCentralWidget(self.tboard)
         self.setWindowIcon(QtGui.QIcon('pacman.ico'))
-        self.resize(800, 800)
+        self.resize(840, 840)
 
-        print(self.pos().x(), " ", self.pos().y())
-
-        self.drawall()
         self.center()
+
         self.show()
-
-    def drawall(self):
-
-        painter = QPainter(self)
-        rect = self.contentsRect()
-        boardTop = rect.bottom() - 800
-        self.draw(boardTop, boardTop, painter, QColor(0x6666CC))
-
-    def draw(self, x, y, painter, color):
-
-        painter.fillRect(x + 1, y + 1, 20 - 2,
-                         20 - 2, color)
-
-        #painter.setPen(color.lighter())
-        painter.drawLine(x, y + 20 - 1, x, y)
-        painter.drawLine(x, y, x + 20 - 1, y)
-
-          #  painter.setPen(color.darker())
-        painter.drawLine(x + 1, y + 20 - 1,
-                         x + 20 - 1, y + 20 - 1)
-        painter.drawLine(x + 20 - 1,
-                         y + 20 - 1, x + 20 - 1, y + 1)
-
-    def squareWidth(self):
-
-        return 20
-
-    def squareHeight(self):
-
-        return 20
 
     def center(self):
         """centers the window on the screen"""
@@ -83,6 +32,69 @@ class Window(QMainWindow):
         size = self.geometry()
         self.move((screen.width() - size.width()) / 2,
                   (screen.height() - size.height()) / 2)
+
+
+class Board(QFrame):
+
+        BoardWidth = 840
+        BoardHeight = 840
+
+        def __init__(self, parent):
+
+            super().__init__(parent)
+            self.resize(800, 800)
+
+            self.tiles = [
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+                    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+                    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+                    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+                    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+                    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+                    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+                    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+                    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+                    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+                    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+                    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+                    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
+                    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+                    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+                    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+                    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                 ]
+
+        def paintEvent(self, event):
+            painter = QPainter(self)
+            rect = self.contentsRect()
+            for x in range(20):
+                for y in range(20):
+                    if self.tiles[x * 20 + y] == 0:
+                         self.draw(x * 42, y * 42, painter, QColor(0x6666CC))
+                    else:
+                        self.draw(x * 42, y * 42, painter, QColor(0xFF0000))
+        def draw(self, x, y, painter, color):
+            painter.fillRect(x + 1, y + 1, 42 - 2,
+                             42 - 2, color)
+
+            # painter.setPen(color.lighter())
+            painter.drawLine(x, y + 42 - 1, x, y)
+            painter.drawLine(x, y, x + 42 - 1, y)
+
+            #  painter.setPen(color.darker())
+            painter.drawLine(x + 1, y + 42 - 1,
+                             x + 42 - 1, y + 42 - 1)
+            painter.drawLine(x + 42 - 1,
+                             y + 42 - 1, x + 42 - 1, y + 1)
+
+        def squareWidth(self):
+            return 20
+
+        def squareHeight(self):
+            return 20
 
 
 if __name__ == '__main__':
