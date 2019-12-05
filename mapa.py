8cf65c1dcf9b150641f0c749c2e8d62090a08c08
Dell <Dell@DESKTOP-5KT8N2B>
2019-12-05 12:50:07 +0100

    Mapa, konacna verzija

--- mapa.py
+++ mapa.py
@@ -20,7 +20,7 @@ class Window(QMainWindow):
 
         self.setCentralWidget(self.tboard)
         self.setWindowIcon(QtGui.QIcon('pacman.ico'))
-        self.resize(840, 840)
+        self.resize(882, 924)
 
         self.center()
         self.show()
@@ -31,65 +31,84 @@ class Window(QMainWindow):
         screen = QDesktopWidget().screenGeometry()
         size = self.geometry()
         self.move((screen.width() - size.width()) / 2,
-                  (screen.height() - size.height()) / 2)
+                  (screen.height() - size.height()) / 2-40)
 
 
 class Board(QFrame):
-    BoardWidth = 800
-    BoardHeight = 800
+    BoardWidth = 882
+    BoardHeight = 924
 
     def __init__(self, parent):
         self.pp = p.Player()
         super().__init__(parent)
-        self.resize(800, 800)
+        self.resize(882, 924)
+        self.shape = 10
 
         self.tiles = [
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-            0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
-            0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-            0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 1,  10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+            10, 3,  12, 12, 9,  12, 5,  10, 10, 10, 1,  10, 10, 10, 3,  12, 5,  10, 3,  12, 5, 10,
+            10, 1,  10, 10, 1,  10, 1,  10, 10, 10, 1,  10, 10, 10, 1,  10, 2,  12, 8,  10, 1, 10,
+            10, 1,  10, 10, 1,  10, 1,  10, 10, 10, 1,  10, 10, 10, 1,  10, 10, 10, 1,  10, 1, 10,
+            10, 6,  12, 12, 11, 12, 7,  12, 12, 12, 11, 12, 12, 12, 11, 12, 9,  12, 4,  10, 1, 10,
+            10, 1,  10, 10, 1,  10, 10, 10, 10, 10, 1,  10, 10, 10, 1,  10, 1,  10, 10, 10, 1, 10,
+            10, 1,  10, 10, 6,  12, 5,  10, 3,  12, 7,  12, 9,  12, 8,  10, 6,  12, 5,  10, 1, 10,
+            10, 1,  10, 10, 1,  10, 1,  10, 1,  10, 10, 10, 1,  10, 1,  10, 1,  10, 1,  10, 1, 10,
+            10, 2,  12, 12, 8,  10, 1,  10, 1,  10, 14, 10, 1,  10, 1,  10, 1,  10, 1,  10, 1, 10,
+            10, 10, 10, 10, 1,  10, 2,  12, 8,  10, 1,  10, 1,  10, 2,  12, 8,  10, 2,  12, 8, 10,
+            10, 10, 10, 10, 1,  10, 10, 10, 6,  12, 8,  10, 1,  10, 10, 10, 1,  10, 10, 10, 1, 10,
+            10, 10, 10, 10, 1,  10, 3,  12, 8,  10, 1,  10, 1,  10, 3,  12, 8,  10, 3,  12, 8, 10,
+            10, 3,  12, 12, 8,  10, 1,  10, 1,  10, 13, 10, 1,  10, 1,  10, 1,  10, 1,  10, 1, 10,
+            10, 1,  10, 10, 1,  10, 1,  10, 1,  10, 10, 10, 1,  10, 1,  10, 1,  10, 1,  10, 1, 10,
+            10, 1,  10, 10, 6,  12, 4,  10, 2,  12, 9,  12, 7,  12, 8,  10, 6,  12, 4,  10, 1, 10,
+            10, 1,  10, 10, 1,  10, 10, 10, 10, 10, 1,  10, 10, 10, 1,  10, 1,  10, 10, 10, 1, 10,
+            10, 6,  12, 12, 11, 12, 9,  12, 12, 12, 11, 12, 12, 12, 11, 12, 7,  12, 5,  10, 1, 10,
+            10, 1,  10, 10, 1,  10, 1,  10, 10, 10, 1,  10, 10, 10, 1,  10, 10, 10, 1,  10, 1, 10,
+            10, 1,  10, 10, 1,  10, 1,  10, 10, 10, 1,  10, 10, 10, 1,  10, 3,  12, 8,  10, 1, 10,
+            10, 2,  12, 12, 7,  12, 4,  10, 10, 10, 1,  10, 10, 10, 2,  12, 4,  10, 2,  12, 4, 10,
+            10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 1,  10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
         ]
 
     def paintEvent(self, event):
         painter = QPainter(self)
         rect = self.contentsRect( )
-        for x in range(20):
-            for y in range(20):
-                if self.tiles[x * 20 + y] == 0:
-                    self.draw(x * 42, y * 42, painter, QColor(0x6666CC))
+        for x in range(21):
+            for y in range(22):
+                self.setshape(self.tiles[x * 22 + y])
+                if self.tiles[x * 22 + y] == 10:
+                    self.draw(x * 42, y * 42, painter, QColor(0x000000))
                 else:
-                    self.draw(x * 42, y * 42, painter, QColor(0xFF0000))
+                    self.draw(x * 42, y * 42, painter, QColor(0x000000))
+
+    def setshape(self,oblik):
+        self.shape = oblik
+
 
     def draw(self, x, y, painter, color):
-        painter.fillRect(x + 1, y + 1, self.squareHeight() - 2,
-                         self.squareWidth() - 2, color)
+        painter.fillRect(x + 1, y + 1, self.squareHeight(),
+                         self.squareWidth(), color)
 
-        # painter.setPen(color.lighter())
-        painter.drawLine(x, y + self.squareWidth() - 1, x, y)
-        painter.drawLine(x, y, x + self.squareHeight() - 1, y)
+        painter.setPen(QColor(0x000099))
+
+        if self.shape == 3 or self.shape == 5 or self.shape == 9 or self.shape == 12 or self.shape == 14:
+            painter.drawLine(x, y + self.squareWidth(), x, y)
+            #gore
+        if self.shape == 2 or self.shape == 1 or self.shape == 3 or self.shape == 6 or self.shape == 13 or self.shape == 14:
+            painter.drawLine(x, y, x + self.squareHeight() - 1, y)
 
         #  painter.setPen(color.darker())
-        painter.drawLine(x + 1, y + self.squareWidth() - 1,
-                         x + self.squareHeight() - 1, y + self.squareWidth() - 1)
-        painter.drawLine(x + self.squareHeight() - 1,
-                         y + self.squareWidth() - 1, x + self.squareHeight() - 1, y + 1)
+        #desna
+
+        if self.shape == 1 or self.shape == 4 or self.shape == 5 or self.shape == 8 or self.shape == 13 or self.shape == 14:
+            painter.drawLine(x + 1, y + self.squareWidth(),
+                         x + self.squareHeight()+1, y + self.squareWidth())
+        #donja
+        if self.shape == 2 or self.shape == 4 or self.shape == 7 or self.shape == 12 or self.shape == 13:
+            painter.drawLine(x + self.squareHeight()+1,
+                         y + self.squareWidth() , x + self.squareHeight(), y )
         self.pp.paint(painter, self)
+
+
+
 
     def squareWidth(self):
         return 42
